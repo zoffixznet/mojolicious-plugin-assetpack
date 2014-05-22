9d726e1b0d7323a0f4459c6e091796673a6f0ef6
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-05-22 22:04:13 +0200

    Add "ESC" to escape presentation mode

--- public/js/impress.js
+++ public/js/impress.js
@@ -711,24 +711,31 @@
         //   as another way to moving to next step... And yes, I know that for the sake of
         //   consistency I should add [shift+tab] as opposite action...
         document.addEventListener("keyup", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-                switch( event.keyCode ) {
-                    case 33: // pg up
-                    case 37: // left
-                    case 38: // up
-                             api.prev();
-                             break;
-                    case 9:  // tab
-                             break;
-                    case 32: // space
-                    case 34: // pg down
-                    case 39: // right
-                    case 40: // down
-                             api.next();
-                             break;
-                }
-                
-                event.preventDefault();
+            var prevent = true;
+
+            switch( event.keyCode ) {
+                case 27:
+                        window.location.href = window.location.href.replace(/present=1/, '').replace(/#\//, '#');
+                        break;
+                case 33: // pg up
+                case 37: // left
+                case 38: // up
+                          api.prev();
+                          break;
+                case 9:  // tab
+                          break;
+                case 32: // space
+                case 34: // pg down
+                case 39: // right
+                case 40: // down
+                          api.next();
+                          break;
+                default:
+                          prevent = false;
+            }
+
+            if(prevent) {
+              event.preventDefault();
             }
         }, false);
         
